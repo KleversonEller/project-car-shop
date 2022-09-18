c1dec6c307280f51e3fda1c427e0b0db47b096a1
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-18 00:06:29 -0300

    refactor: :recycle: Implementando validaçoes
    Implementando validaçoes utilizando o Zod

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -1,6 +1,9 @@
-import { IVehicle } from './IVehicle';
+import { z } from 'zod';
+import { IVehicleSchema } from './IVehicle';
 
-export interface ICar extends IVehicle {
-  doorsQty: number; // deve ter validação de >= 2 e <= 4 e ser um inteiro positivo
-  seatsQty: number; // deve ter validação de >= 2 e <= 7 e ser um inteiro positivo
-}
+export const ICarSchema = IVehicleSchema.extend({
+  doorsQty: z.number().lte(4).gte(2).int(), // deve ter validação de >= 2 e <= 4 e ser um inteiro positivo
+  seatsQty: z.number().lte(7).gte(2).int(), // deve ter validação de >= 2 e <= 7 e ser um inteiro positivo
+});
+
+export type ICar = z.infer<typeof ICarSchema>;
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,7 +1,11 @@
-export interface IVehicle {
-  model: string; // deve ter validação de tres caracteres
-  year: number; // deve ter validação de >= 1900 e <= 2022 e ser um inteiro positivo
-  color: string; // deve ter validação de tres caracteres
-  status?: boolean; 
-  buyValue: number; // deve receber apenas numeros inteiros
-}
+import { z } from 'zod';
+
+export const IVehicleSchema = z.object({
+  model: z.string().min(3), // deve ter validação de tres caracteres
+  year: z.number().lte(2022).gte(1900).int(), // deve ter validação de >= 1900 e <= 2022 e ser um inteiro positivo
+  color: z.string().min(3), // deve ter validação de tres caracteres
+  status: z.boolean().optional(), 
+  buyValue: z.number().int(), // deve receber apenas numeros inteiros
+});
+
+export type IVehicle = z.infer<typeof IVehicleSchema>;
