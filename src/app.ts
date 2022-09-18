caec07685a7aac1b2df1e60cedba98d64bfc40e1
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-18 00:12:56 -0300

    feat: :sparkles: Criando routers (/cars)
    Criando a rota 'POST/cars' que cria um novo documento na tabela Cars

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import 'express-async-errors';
+import errorMiddleware from './middleware/error.middleware';
+import carRouter from './routers/car.routes';
 
 const app = express();
+app.use(express.json());
+
+app.use(carRouter);
+
+app.use(errorMiddleware);
 
 export default app;
---
+++ src/routers/car.routes.ts
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import CarsModel from '../models/car.model';
+import CarsService from '../services/car.service';
+import CarsController from '../controllers/car.controller';
+
+const carRouter = Router();
+const carModel = new CarsModel();
+const carService = new CarsService(carModel);
+const carController = new CarsController(carService);
+
+carRouter.post('/cars', (req, res) => carController.create(req, res));
+
+export default carRouter;
