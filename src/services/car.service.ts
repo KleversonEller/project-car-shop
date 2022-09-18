92438dc77b0e9abbd0e2b5b67515c094aa074740
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-18 00:10:05 -0300

    feat: :sparkles: Criando service (CarsService)
    Criando service para implementação de regras para criação de um novo documento na tabela Cars

---
+++ src/services/car.service.ts
@@ -0,0 +1,20 @@
+import { ICar, ICarSchema } from '../interfaces/ICar';
+import IService from '../interfaces/IService';
+import { IModel } from '../interfaces/IModel';
+import PersonalError from '../util/personal.error';
+
+export default class CarsService implements IService<ICar> {
+  private _cars: IModel<ICar>;
+
+  constructor(model: IModel<ICar>) {
+    this._cars = model;
+  }
+
+  public async create(obj: ICar): Promise<ICar> {
+    const valid = ICarSchema.safeParse(obj);
+    if (!valid.success) throw new PersonalError(400, 'Fields is invalid !');
+
+    const result = await this._cars.create(obj);
+    return result;
+  }
+}
