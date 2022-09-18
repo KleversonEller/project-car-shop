06ae276e4c76dedceb73c6b01d9170bab1471b95
Kleverson Eller <kleverson.a16@gmail.com>
2022-09-18 00:38:49 -0300

    test: :test_tube: Criando testes (Model)
    Criando testes unitarios para a camada model

---
+++ src/tests/unit/models/mock.car.model.ts
@@ -0,0 +1,13 @@
+import { ICar } from '../../../interfaces/ICar'
+
+const mockCreate: ICar & {_id: string} = {
+    _id: "4edd40c86762e0fb12000003",
+    model: "Ferrari Maranello",
+    year: 1963,
+    color: "red",
+    buyValue: 3500000,
+    seatsQty: 2,
+    doorsQty: 2
+}
+
+export { mockCreate }
---
+++ src/tests/unit/models/teste-model.test.ts
@@ -0,0 +1,25 @@
+import * as sinon from 'sinon';
+import chai from 'chai';
+import CarsModel from '../../../models/car.model';
+import { Model } from 'mongoose';
+import { mockCreate } from './mock.car.model';
+
+const { expect } = chai;
+
+describe('Cars Model', () => {
+  const carModel = new  CarsModel();
+
+  before(async () => {
+    sinon.stub(Model, 'create').resolves(mockCreate);
+  });
+
+  after(()=>{
+    sinon.restore();
+  })
+
+  it('Success create', async () => {
+    const result = await carModel.create(mockCreate);
+    expect(result).to.be.deep.equal(mockCreate);
+  });
+
+});
